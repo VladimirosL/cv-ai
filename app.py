59201bee7a7833478382d31fd196952bc11f996f
Vlad <vlalam010413@student.jenseneducation.se>
2026-09-10 08:21:26 +0200

    initialise DB on startup

--- app.py
+++ app.py
@@ -1283,10 +1283,7 @@ def download_cv():
 # Starta app
 # --------------------------------------------------
 
+init_db()
+
 if __name__ == "__main__":
-
-    init_db()
-
-    app.run(
-        debug=True
-    )
+    app.run(debug=True)
